324b9e3bd8a6f39a78a83e14ff8ed129b6f23913
Raymond Mariano <rcm@hawaii.edu>
2015-05-22 14:17:24 -1000

    FINISHED

--- elevator.js
+++ elevator.js
@@ -2,5 +2,18 @@
 'use strict';
 
 module.exports = {
+goto: _goto
+}
 
-};
+function _goto(lvl, btn){
+  if(typeof lvl !== 'number' || lvl > 3 || lvl < 0){
+    return 0;
+  }
+  if(typeof btn !== 'string' || Number(btn) > 3 || Number(btn) < 0){
+    return 0;
+  }
+
+  var dif = (lvl - Number(btn)) *-1;
+
+  return dif;
+}
